447d659116b02b62fa8581f931395962b9884452
SharanVarma25 <boredasharan400@gmail.com>
2025-01-11 21:10:05 +0530

    new files

---
+++ Array.js
@@ -0,0 +1,121 @@
+// //
+//  const user1 = "sharan";
+// const user2 = "shashi";
+// const user3 = "arun";
+// const user4 = "mani";
+// const user5 ="manish";
+// console.log(user1); 
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+// console.log(user5);
+
+// //
+// const users = ["sharan","shashi","mani","arun","manish"]
+// console.log(users[0]);
+// console.log(users[3]);
+// for(let i =0;i< users.length;i++){
+//     console.log(users[i]);
+// }
+
+////
+// const users = ["sharan","shashi","mani","arun","manish"]
+// push method
+//  users.push("Arun");
+//  console.log(users);
+
+// pop method
+//  users.pop();
+//  console.log(users);
+
+// shift
+// users.shift();
+// console.log(users);
+
+// //join
+// const users = ["sharan","shashi","mani","arun","manish"];
+// let x = users.join("");
+// console.log(x , typeof(x));
+
+// //concat
+// let frontend = ["HTML" , "CSS" , "JS" , "REACTJS" , "ANGULAR" ,"VUE"]; 
+// let backend = ["NodeJs" , "Expressjs" , "Java" , "PHP ", "Golang"];
+// let db = ["MongoDB" , "MySQL" , "Oracle", "MariaDB", "CouchDB"];
+// console.log(frontend);
+// console.log(backend);
+
+// let fullstack = frontend. concat(backend);
+// console.log(fullstack);
+
+// //! objects
+// let userDetails = {
+//     name:"shashi",
+//     age:18,
+//     city:"cheriyal",
+// };
+// console.log(userDetails);
+// console.log(userDetails.name);
+
+// //nested object
+// let userDetails = {
+//     name:"Shashi",
+//     age:20,
+//     city:"Hyd",
+//     address:{
+//         area:"doulthabad",
+//         street:{
+//             streetNo:1,
+//             pinCode:502296
+//         }
+//     }
+// };
+// console.log(userDetails.address.street.pinCode);
+
+// ! create  a  array of objects
+// let user1 ={
+//     name:"manish",
+//     city:"NewYork"
+// }
+// let user2 ={
+//     name:"mani",
+//     city:"USA"
+// }
+// let user3 ={
+//     name:"arun",
+//     city:"India"
+// }
+// let user4 ={
+//     name:"Shashi",
+//     city:"India"
+// }
+// console.log(user1.name);
+// console.log(user2.name);
+// console.log(user3.name);
+// console.log(user4.name);
+
+
+// let users = [
+//     {
+//         name:"manish",
+//         city:"NewYork"
+//     },
+//     {
+//         name:"mani",
+//         city:"USA"
+//     },
+//     {
+//         name:"arun",
+//         city:"India"
+//     },
+//     {
+//         name:"shashi",
+//         city:"India"
+//     },
+//     {
+//         name:"John",
+//         city:"Chennai"
+//     }
+// ];
+// users.map((user)=>{
+//     console.log(user.name);
+// })
