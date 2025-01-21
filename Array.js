a3984d7e3140fafcdca403fdbe5480771e801336
SharanVarma25 <boredasharan400@gmail.com>
2025-01-21 09:37:52 +0530

    newfiles pushed

--- Array.js
+++ Array.js
@@ -119,3 +119,22 @@
 // users.map((user)=>{
 //     console.log(user.name);
 // })
+
+
+// let userDetails = {
+//     name : "Ankith",
+//     age:25,
+//     city:"Hyderabad"
+// }
+//     console.log(userDetails);
+//     console.log(userDetails.name);
+//     console.log(typeof(userDetails));
+    
+//     let x = JSON.stringify(userDetails);
+//     console.log(x);
+//     console.log(x.name);
+//     console.log(typeof(x));
+    
+//     let y = JSON.parse(x);
+//     console.log(y);
+
