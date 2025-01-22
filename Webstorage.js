59528c25f56f4c383c1ac77b5ca19aed08e93f60
sharanvarma25 <149095157+sharanvarma25@users.noreply.github.com>
2025-01-22 12:30:10 +0530

    Add files via upload

---
+++ Webstorage.js
@@ -0,0 +1,12 @@
+//
+localStorage.setItem("user1" , "Raghu");
+localStorage.setItem("user2","Vamshi")
+localStorage.setItem("user3" , "harsha");
+localStorage.setItem("user4","Sanjay")
+localStorage.setItem("user5" , "shashi");
+localStorage.setItem("user6","govardhan");
+
+let user6 = localStorage.getItem("user6");
+console.log(user6);
+
+localStorage.removeItem("user1");
