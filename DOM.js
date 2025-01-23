6c0bc422cbf30777922dc66c63a0a9c16c924f30
sharanvarma25 <149095157+sharanvarma25@users.noreply.github.com>
2025-01-23 10:33:01 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,9 @@
+//  document.getElementById("")
+let element = document.getElementById("demo");
+element.innerText = "DOM";
+console.log(element);
+
+let div = document.getElementById("test");
+// div.inmerText = "<h1>Header</h1>"
+div.inmerHTML = "<h1>Header</h1>"
+console.log(div);
