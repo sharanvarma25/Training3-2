45c306dbcc25c3146505f7c94bd00fa74a0af371
sharanvarma25 <149095157+sharanvarma25@users.noreply.github.com>
2025-01-24 10:30:06 +0530

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -1,9 +1,23 @@
-//  document.getElementById("")
-let element = document.getElementById("demo");
-element.innerText = "DOM";
-console.log(element);
+// //  document.getElementById("")
+// let element = document.getElementById("demo");
+// element.innerText = "DOM";
+// console.log(element);
 
-let div = document.getElementById("test");
-// div.inmerText = "<h1>Header</h1>"
-div.inmerHTML = "<h1>Header</h1>"
-console.log(div);
+// let div = document.getElementById("test");
+// // div.inmerText = "<h1>Header</h1>"
+// div.inmerHTML = "<h1>Header</h1>"
+// console.log(div);
+
+// //
+// let ele = document.getElementsByClassName("test");
+// ele[0].style.backgroundColor="yellow"
+// let x = [ ... ele];
+// console.log(x , Array.isArray(x));
+// x.map((element)=>{
+//     element.style.backgroundColor="teal";
+// })
+
+let ele = document.querySelectorAll(".test");
+[...ele].map(element=>{
+    console.log(element.innerText);
+})
