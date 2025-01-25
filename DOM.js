caafcc1b48282735e4a27c35ae34af637c96c22f
sharanvarma25 <149095157+sharanvarma25@users.noreply.github.com>
2025-01-25 21:44:46 +0530

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -3,8 +3,9 @@
 // element.innerText = "DOM";
 // console.log(element);
 
+////
 // let div = document.getElementById("test");
-// // div.inmerText = "<h1>Header</h1>"
+// div.inmerText = "<h1>Header</h1>"
 // div.inmerHTML = "<h1>Header</h1>"
 // console.log(div);
 
@@ -17,7 +18,53 @@
 //     element.style.backgroundColor="teal";
 // })
 
-let ele = document.querySelectorAll(".test");
-[...ele].map(element=>{
-    console.log(element.innerText);
-})
+// let ele = document.querySelectorAll(".test");
+// [...ele].map(element=>{
+//     console.log(element.innerText);
+// })
+
+
+// // ! events
+// let btn = document.getElementById("btn");
+// // btn.addEventListener("events" , callbackfn)
+// btn.addEventListener("click",()=>{
+//     console.log("button clicked");
+//     document.body.style.background="dodgerblue"
+// })
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("dblclick",()=>{
+//     console.log("button is clicked 2 times");
+// })
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("mouseover" , ()=>{
+//     // alert("mouse over done")
+//     document.body.style.background="teal"
+// })
+
+// btn.addEventListener("mouseleave" , ()=>{
+//     // alert("mouse leave done")
+//     document.body.style.background="tomato"
+// })
+
+
+// let inp = document.querySelector("#inp");
+// inp.addEventListener("keydown",()=>{
+//     console.log("key down");
+// })
+
+// inp.addEventListener("keyup",()=>{
+//     console.log("key up");
+// })
+
+// //
+// let bgColor = document.querySelectorAll(".bgColor");
+// [...bgColor].map((element)=>{
+//     element.addEventListener("mouseover",()=>{
+//         element.style.backgroundColor=element.innerText;
+//     });
+//     element.addEventListener("mouseleave",()=>{
+//         element.style.backgroundColor="transparent";
+//     })
+// })
